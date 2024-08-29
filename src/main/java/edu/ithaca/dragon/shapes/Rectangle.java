53b94716e2caa97766a20d616f307e41701e6b1a
Finn <36242395+ploorp@users.noreply.github.com>
2024-08-29 15:23:40 -0400

    finish constructor

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -4,21 +4,32 @@ public class Rectangle {
     private double length;
     private double width;
     
-
+    /**
+     * @throws IllegalArgumentException if length or width is not a positive number
+     */
     public Rectangle(double length, double width){
         if (length <= 0 || width <= 0) {throw new IllegalArgumentException();}
-        self.length = length;
-        self.width = width;
+        this.length = length;
+        this.width = width;
     }
 
+    /**
+     * @return the area of this rectangle
+     */
     public double calcArea(){
        throw new RuntimeException("Not implemented yet");
     }
 
+    /**
+     * @post doubles the size of this rectangle
+     */
     public void doubleSize(){
         throw new RuntimeException("Not implemented yet");
     }
 
+    /**
+     * @return the longest straight line that can be drawn within this rectangle
+     */
     public double longestLineWithin(){
         throw new RuntimeException("Not implemented yet");
     }
