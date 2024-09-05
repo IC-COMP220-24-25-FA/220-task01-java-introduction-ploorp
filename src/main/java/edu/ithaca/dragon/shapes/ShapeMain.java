7d9afea831a9684b03cc097b6e8839efe8814811
Finn <36242395+ploorp@users.noreply.github.com>
2024-09-05 18:52:03 -0400

    a

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -44,12 +44,10 @@ public class ShapeMain {
     public static void main(String[] args){
         generateShapes();
 
-        
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
 
-        /*
         Random rand = new Random();
 
         Rectangle[] rectangles = new Rectangle[5];
@@ -82,6 +80,5 @@ public class ShapeMain {
         }
 
         input.close();
-        */
     }
 }
